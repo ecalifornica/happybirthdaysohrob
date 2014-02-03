c4682c22489008d7762347c0d26f697e0ad8949b
ecalifornica <queenin+git@gmail.com>
2014-02-03 15:09:48 -0800

    vote form

--- app.py
+++ app.py
@@ -208,10 +208,10 @@ def vote():
         # Is there a better way to make this query?
         sql_user = sql_session.query(User).filter_by(twitter_screen_name=current_user.id).first()
 
-        #sql_user.mattress_vote = request.form['mattress_vote']
+        sql_user.mattress_vote = request.form['mattress_vote']
         print(sql_user)
-        #print('MATTRESS VOTE: %s' % sql_user.mattress_vote
-        #sql_session.commit()
+        print('MATTRESS VOTE: %s' % sql_user.mattress_vote
+        sql_session.commit()
 
     #return render_template('index.html')
     return ('WTF')
