6041041bee898c51542a6b4abca60b899f412e57
ecalifornica <queenin+git@gmail.com>
2014-02-06 05:52:00 -0800

    badge user vote

--- app.py
+++ app.py
@@ -157,10 +157,13 @@ def create_donut_csv(csv_handle, total_goal):
 
 def bit_bang_donor_string():
     donor_html_string = ''
+    vote_badge_class = ''
     for row in sql_session.query(User):
         print('ROW.PLEDGE_AMOUNT: %s' % row.pledge_amount)
         if row.pledge_amount is not 0 and row.stripe_token is not None:
-            donor_html_string += '<div class="col-md-3"><p><img width="73px" src="https://s3.amazonaws.com/happybirthdaysohrob/%s" class="img-rounded"></p><p style="margin-top:-5px; margin-bottom:-5px;font-family:Helvetica"><a href="http://www.twitter.com/%s">@%s</a></p><p style="font-weight:200;">$%s Vote: %s</p></div>' % (row.twitter_photo, row.twitter_screen_name, row.twitter_screen_name, row.pledge_amount, row.mattress_vote)
+            if row.mattress_vote is not None and row.mattress_vote == 3:
+                vote_badge_class = 'color: orange;'
+            donor_html_string += '<div class="col-md-3"><p><img width="73px" src="https://s3.amazonaws.com/happybirthdaysohrob/%s" class="img-rounded"></p><p style="margin-top:-5px; margin-bottom:-5px;font-family:Helvetica"><a href="http://www.twitter.com/%s">@%s</a></p><p style="font-weight:700;%s">$%s Vote: %s</p></div>' % (row.twitter_photo, row.twitter_screen_name, row.twitter_screen_name, vote_badge_class, row.pledge_amount, row.mattress_vote)
     return donor_html_string
 
 
