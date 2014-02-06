d61a429b067d118a744543525d5346751b861860
ecalifornica <queenin+git@gmail.com>
2014-02-06 06:18:45 -0800

    vote colors

--- app.py
+++ app.py
@@ -152,14 +152,15 @@ def create_data_csv(csv_handle, total_goal):
         return total_pledges
 
 
+mattress_color = ['', '#E453EE;', '#5EEE53;', '#EEE453;']
 def bit_bang_donor_string():
     donor_html_string = ''
     vote_badge_class = ''
     for row in sql_session.query(User):
         print('ROW.PLEDGE_AMOUNT: %s' % row.pledge_amount)
         if row.pledge_amount is not 0 and row.stripe_token is not None:
-            if row.mattress_vote is not None and row.mattress_vote == 3:
-                vote_badge_class = 'color: orange;'
+            if row.mattress_vote is not None:
+                vote_badge_class = mattress_color[int(row.mattress_vote)]
             donor_html_string += '<div class="col-md-3"><p><img width="73px" src="https://s3.amazonaws.com/happybirthdaysohrob/%s" class="img-rounded"></p><p style="margin-top:-5px; margin-bottom:-5px;font-family:Helvetica"><a href="http://www.twitter.com/%s">@%s</a></p><p style="font-weight:700;%s">$%s Vote: %s</p></div>' % (row.twitter_photo, row.twitter_screen_name, row.twitter_screen_name, vote_badge_class, row.pledge_amount, row.mattress_vote)
     return donor_html_string
 
