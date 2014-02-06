7be69be609f521d9efaa7a3f39e742dcd5dbb8f7
ecalifornica <queenin+git@gmail.com>
2014-02-06 06:22:04 -0800

    vote colors

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def bit_bang_donor_string():
         if row.pledge_amount is not 0 and row.stripe_token is not None:
             if row.mattress_vote is not None:
                 vote_badge_class = mattress_color[int(row.mattress_vote)]
-            donor_html_string += '<div class="col-md-3"><p><img width="73px" src="https://s3.amazonaws.com/happybirthdaysohrob/%s" class="img-rounded"></p><p style="margin-top:-5px; margin-bottom:-5px;font-family:Helvetica"><a href="http://www.twitter.com/%s">@%s</a></p><p style="font-weight:700;%s">$%s Vote: %s</p></div>' % (row.twitter_photo, row.twitter_screen_name, row.twitter_screen_name, vote_badge_class, row.pledge_amount, row.mattress_vote)
+            donor_html_string += '<div class="col-md-3"><p><img width="73px" src="https://s3.amazonaws.com/happybirthdaysohrob/%s" class="img-rounded"></p><p style="margin-top:-5px; margin-bottom:-5px;font-family:Helvetica"><a href="http://www.twitter.com/%s">@%s</a></p><p style="font-weight:700;color:%s">$%s Vote: %s</p></div>' % (row.twitter_photo, row.twitter_screen_name, row.twitter_screen_name, vote_badge_class, row.pledge_amount, row.mattress_vote)
     return donor_html_string
 
 
@@ -201,9 +201,9 @@ def index():
         except:
             pass
         if sql_user.mattress_vote == 1:
-            vote_one_classes = 'btn-success'
+            vote_one_classes = 'btn-success mattress-one'
         if sql_user.mattress_vote == 2:
-            vote_two_classes = 'btn-success'
+            vote_two_classes = 'btn-success mattress-two'
         if sql_user.mattress_vote == 3:
             vote_three_classes = 'btn-success mattress_three'
 
