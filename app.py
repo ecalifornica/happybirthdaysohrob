e5c750d803a37d81a221612b72ed112d281bc4c4
ecalifornica <queenin+git@gmail.com>
2014-02-14 11:55:44 -0800

    refactor login route

--- app.py
+++ app.py
@@ -132,9 +132,11 @@ def vote():
 
 @app.route('/login/')
 def twitter():
+    print('login')
     if oauth_dancer is None:
+        print('oauth is none')
         oauth_dancer = oauth_placeholder(consumer_key, consumer_secret, callback_url)
-
+    print(oauth_dancer)
     if not request.args.get('oauth_token'):
         oauth_dancer.auth = tweepy.OAuthHandler(oauth_dancer.consumer_key, oauth_dancer.consumer_secret, oauth_dancer.callback_url)
         oauth_dancer.auth.secure = True
